65d3440fb1bcb6243e90e241a50bd5fdb6c89d54
Poroburu <poroburu@gmail.com>
2025-05-08 12:30:52 -0400

    Update app metadata: change title to 'Poroburu' and description to 'Poroburu Validator'

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Poroburu',
+  description: 'Poroburu Validator',
 }
 import { ThemeProvider } from "next-themes"
 
